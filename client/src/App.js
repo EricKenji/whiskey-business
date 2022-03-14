4d9e6ec0e6342a54bd8d4977a87168a365535619
Eric Furukawa <eric.furukawa@gmail.com>
2022-03-14 13:22:24 -0700

    debugged and imported pages to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Login';
+import Search from './pages/Login';
+import Signup from './pages/Login';
+
 
 const httpLink = createHttpLink({
   uri: '/graphql'
@@ -27,7 +33,11 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <Switch>
-          <Route></Route>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/profile' component={Profile} />
+          <Route exact path='/search' component={Search} />
+          <Route exact path='/signup' component={Signup} />
         </Switch>
       </Router>
     </ApolloProvider>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="container">
+      test
     </div>
   );
 };
