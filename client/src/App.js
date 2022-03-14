4a87e091ed577b7e353dba36da02de1b195a9b20
brandon.sauer <brantheman8@comcast.net>
2022-03-13 20:19:00 -0700

    added to footer and header components

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,17 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+import Search from './pages/Search'
+
 const httpLink = createHttpLink({
-  uri: 'graphql'
+  uri: '/graphql'
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -26,7 +35,19 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-
+      <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/search" component={Search} />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
       </Router>
     </ApolloProvider>
   );
