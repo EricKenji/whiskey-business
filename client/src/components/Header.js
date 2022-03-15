0cda12aaee2016ec25281ba4cdd6b713b8c1037c
Opal <guerre5lily@gmail.com>
2022-03-14 21:28:47 -0700

    add buttons

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Auth from '../utils/auth';
-
+import { Flex, HStack, Heading, Box, Spacer,  } from '@chakra-ui/layout'
+import { Button } from '@chakra-ui/button'
+import { FaGlassWhiskey } from 'react-icons/fa'
 const Header = () => {
   const logout = event => {
     event.preventDefault();
@@ -10,12 +12,17 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <Link to="/">
-          <h1>Whiskey Business</h1>
-        </Link>
-
+    <HStack>
+      <Flex w="100%"  bgGradient="linear(to-r, pink.500, white)" mb="50">      
+        <Box mb="10">
+          <Link to="/">
+            <Heading ml="8" size ="md" fontWeight="semibold" color="white">Whiskey Business
+              <FaGlassWhiskey></FaGlassWhiskey>
+            </Heading>
+           </Link>
+        </Box>
+      <Spacer />
+      <Box mr="8">
         <nav className="text-center">
           {Auth.loggedIn() ? (
             <>
@@ -26,13 +33,19 @@ const Header = () => {
             </>
           ) : (
             <>
-              <Link to="/login">Login</Link>
+              <Button colorScheme='pink' size='sm' mr="2" mt="2">
+                <Link to="/login">Login</Link>  
+              </Button>
+              <Button colorScheme='pink' size='sm' mt="2" >
               <Link to="/signup">Signup</Link>
+              </Button>
             </>
           )}
         </nav>
-      </div>
-    </header>
+      </Box>
+    
+    </Flex>
+    </HStack>
   );
 };
 
