b1842bdc6c79df0780840d6aeb766e08df7780c0
Opal <guerre5lily@gmail.com>
2022-03-15 20:18:07 -0700

    added input fields

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,6 +5,8 @@ import Auth from '../utils/auth';
 import { Flex, HStack, Heading, Box, Spacer,  } from '@chakra-ui/layout'
 import { Button } from '@chakra-ui/button'
 import { FaGlassWhiskey } from 'react-icons/fa'
+import { Image } from '@chakra-ui/react'
+
 const Header = () => {
   const logout = event => {
     event.preventDefault();
@@ -12,7 +14,7 @@ const Header = () => {
   };
 
   return (
-    <HStack boxShadow='l' p='6' rounded='md' bg='white' bgGradient="linear(to-r, orange.300, white)" >
+    <HStack boxShadow='l' p='6' rounded='md' bg='white' bgGradient="linear(to-r, orange.300, white)" > 
       <Flex w="100%"  bgGradient="linear(to-r, orange.300, white)" >      
         <Box >
           <Link to="/">
