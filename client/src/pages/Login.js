b72611a572eded78b15d2369ddece9116994a6dd
brandon.sauer <brantheman8@comcast.net>
2022-03-15 20:16:35 -0700

    added styling to login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { LOGIN_USER } from '../utils/mutations';
-import Auth from '../utils/auth';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
+import { LOGIN_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
+import { Flex, VStack, Box, Spacer, Text } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
 
 function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error }] = useMutation(LOGIN_USER);
 
   const handleFormSubmit = async (event) => {
@@ -30,41 +32,48 @@ function Login(props) {
   };
 
   return (
-    <div className="container">
-      <Link to="/signup">← Go to Signup</Link>
-
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
+    <VStack>
+      <div className="container my-1">
+        <Box mt="4" mb="4">
+          <Button color="orange.300" variant="outline">
+            <Link to="/signup">← Go to Signup</Link>
+          </Button>
+        </Box>
+        <Text fontWeight='bold' color="orange.300" mb="5" fontSize="lg" >Signup</Text>
+        <form onSubmit={handleFormSubmit}>
+          <div className="flex-row space-between my-2">
+        <Text fontWeight='semibold' color="orange.300">Email:</Text>
+            <input
+              placeholder="youremail@test.com"
+              name="email"
+              type="email"
+              id="email"
+              onChange={handleChange}
+            />
           </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+          <div className="flex-row space-between my-2">
+          <Text fontWeight='semibold' color="orange.300">Password:</Text>
+            <input
+              placeholder="******"
+              name="password"
+              type="password"
+              id="pwd"
+              onChange={handleChange}
+            />
+          </div>
+          {error ? (
+            <div>
+              <p className="error-text">
+                The provided credentials are incorrect
+              </p>
+            </div>
+          ) : null}
+          <div className="flex-row flex-end">
+          <Button colorScheme='orange' size='md' mt="2" >Submit</Button>
+          </div>
+        </form>
+      </div>
+    </VStack>
   );
 }
 
