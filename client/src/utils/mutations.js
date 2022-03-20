45ae5b5b080ff71e6aabf904ddebeabfdf918c0b
Robert Sandoval <robsand1992@yahoo.com>
2022-03-19 23:38:38 -0700

    Revert "updated mutatations"
    This reverts commit 706c46910aebb36c202ab7978362741af125198b.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -75,8 +75,14 @@ export const REMOVE_DRINK = gql`
             _id
             email
             savedDrinks {
+<<<<<<< HEAD
+                idDrink
+                name
+                ingredients
+=======
                 drinkId
                 title
+>>>>>>> be763837ccfc1f75ba04641d8d45d0210b18e3b8
                 instructions
                 image
                 glass
